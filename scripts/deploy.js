9d6924e245aec0456741545189de11a4463cee8c
mafaz <mafaz.ahsan@gmail.com>
2023-04-16 09:18:44 +0200

    implemented the full-stack dapp on the hardhat network

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,5 +8,3 @@ const main = async () => {
 main()
   .then(() => console.log("Success"))
   .catch((error) => console.log("Failure", error));
-
-// CONTRACT_ADDRESS = 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
--- src/App.js
+++ src/App.js
@@ -1,5 +1,73 @@
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import Greet from "./artifacts/contracts/Greet.sol/Greet.json";
+
+const HARDHAT_CONTRACT_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+
 function App() {
-  return <div>Mafaz</div>;
+  const [message, setMessage] = useState("");
+  const [msg, setMsg] = useState("");
+
+  useEffect(() => {
+    fetchGreeting();
+  }, []);
+
+  const fetchGreeting = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        Greet.abi,
+        provider
+      );
+      try {
+        const message = await contract.greet();
+        setMessage(message);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  const requestAccount = async () => {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log(accounts);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleUpdate = async () => {
+    if (msg && typeof window.ethereum !== "undefined") {
+      requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        HARDHAT_CONTRACT_ADDRESS,
+        Greet.abi,
+        signer
+      );
+      try {
+        const tx = await contract.update(msg);
+        await tx.wait();
+        fetchGreeting();
+        setMsg("");
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  return (
+    <>
+      <div>{message}</div>
+      <input value={msg} onChange={(event) => setMsg(event.target.value)} />
+      <button onClick={handleUpdate}>update</button>
+    </>
+  );
 }
 
 export default App;
