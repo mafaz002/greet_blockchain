541d2dc593f7b2d4ad1b325ba6ee6812bcdf1eb3
mafaz <mafaz.ahsan@gmail.com>
2023-04-16 07:24:58 +0200

    implemented greet smart contract

---
+++ contracts/Greet.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Greet {
+    string public message;
+
+    constructor(string memory _message) {
+        console.log("Constructor...", _message);
+        message = _message;
+    }
+
+    function greet() view puclic returns (string memory) {
+        return message;
+    }
+
+    function update(string memory _newMessage) public {
+        console.log("Update...", _newMessage);
+        message = _newMessage;
+    }
+}
